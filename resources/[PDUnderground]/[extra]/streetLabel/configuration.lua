efd788a89cf1f144862b89a3e1f888dd1d94f5f4
VPS <abc@mail.com>
2022-02-18 15:27:48 -0800

    add script

---
+++ resources/[PDUnderground]/[extra]/streetLabel/configuration.lua
@@ -0,0 +1,15 @@
+config = {
+	-- Customization options
+	color = '#f0c850';
+
+	position = {
+		-- 0:100
+		offsetX = 20;
+		offsetY = 25; 
+	};
+
+  scale = 1.0;
+
+	vehicleCheck = false; -- Rather or not to display HUD when player(s) are inside a vehicle
+  dynamic = false;
+}
